cdc72248aaf24080f7704c61319ba6d61c9f5953
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-06-18 22:56:05 -0700

    Create csv files and skeleton

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // document.getElementsByClassName("add-number")[0].addEventListener("click", addNumber);
+  // document.getElementsByClassName("remove-number")[0].addEventListener("click", removeNumber);
+  // chart();
+  console.log("hello")
+  tutorial();
+  console.log("after")
+  // setInterval(addNumber, 2000);
+});
